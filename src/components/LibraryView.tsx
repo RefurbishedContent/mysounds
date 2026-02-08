e470b23d4abf14d4ba1f45781813ab360352b5a7
CHEEKY DEV <refurbishedcontent@gmail.com>
2026-02-07 21:37:14 -0500

    Updated LibraryView.tsx

--- src/components/LibraryView.tsx
+++ src/components/LibraryView.tsx
@@ -393,7 +393,7 @@ const LibraryView: React.FC<LibraryViewProps> = ({ onCreateTransitionWithSong, o
             )}
           </>
         ) : (
-          <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-3">
+          <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-3">
             {blends.map((blend) => (
               <div
                 key={blend.id}
