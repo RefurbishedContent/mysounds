2d7051c7a53c54279487688bae5923b3a8bd8b83
CHEEKY DEV <refurbishedcontent@gmail.com>
2026-02-07 21:53:31 -0500

    Updated LabsView.tsx

--- src/components/LabsView.tsx
+++ src/components/LabsView.tsx
@@ -72,7 +72,7 @@ const LabsView: React.FC<LabsViewProps> = ({ onSelectTool }) => {
 
         <div className="p-3 md:p-4">
           <div className="max-w-4xl mx-auto">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+            <div className="grid grid-cols-2 lg:grid-cols-3 gap-3">
               {tools.map((tool) => {
                 const Icon = tool.icon;
                 return (
